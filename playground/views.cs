00d730e9c3bb09c1eff1c86166bc57e9d5ea867b
exyi <exyi@outlook.cz>
2015-02-27 16:41:02 +0100

    included missing files and removed a mess

---
+++ playground/views.cs
@@ -0,0 +1,97 @@
+﻿using RaptorDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace playground
+{
+    public class ModelItem
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string WebSite { get; set; }
+        public Guid Id { get; set; }
+        public string[] Friends { get; set; }
+    
+	    public ModelItem(){
+	    	 Friends= new string[0];
+	    }
+    }
+
+    public class OtherItem
+    {
+        public int[] Numbers;
+        public Dictionary<string, byte[]> Bytes;
+    }
+
+    public class FriendsModelItemView : View<ModelItem>
+    {
+        public class RowSchema : RDBSchema
+        {
+            public string Name;
+        }
+        public FriendsModelItemView()
+        {
+            this.Name = "friends";
+            this.Description = "ModelItems friens";
+            this.ConsistentSaveToThisView = true;
+            this.isActive = true;
+            this.BackgroundIndexing = false;
+            this.Version = 1;
+            // // uncomment the following for transaction mode
+            //this.TransactionMode = true;
+
+            this.Schema = typeof(RowSchema);
+
+            //this.FullTextColumns.Add("customername"); // this or the attribute
+            //this.FullTextColumns.Add("address");
+
+            this.CaseInsensitiveColumns.Add("Name"); // this or the attribute
+            //this.StringIndexLength.Add("nocase", 255);
+
+            this.Mapper = (api, docid, doc) =>
+            {
+                foreach (var f in doc.Friends)
+                {
+                    api.Emit(docid, f);
+                }
+            };
+        }
+
+    }
+
+    public class DefaultModelItemView : RaptorDB.View<ModelItem>
+    {
+        public class RowSchema : RDBSchema
+        {
+            public string Name;
+            public int Number;
+        }
+        public DefaultModelItemView()
+        {
+            this.Name = "ModelItem";
+            this.Description = "A primary view for ModelItem";
+            this.isPrimaryList = true;
+            this.isActive = true;
+            this.BackgroundIndexing = false;
+            this.Version = 1;
+            //// uncomment the following for transaction mode
+            //this.TransactionMode = true;
+
+            this.Schema = typeof(RowSchema);
+
+            //this.FullTextColumns.Add("customername"); // this or the attribute
+            //this.FullTextColumns.Add("address");
+
+            this.CaseInsensitiveColumns.Add("Name"); // this or the attribute
+            //this.StringIndexLength.Add("nocase", 255);
+
+            this.Mapper = (api, docid, doc) =>
+            {
+                api.EmitObject(docid, doc);
+            };
+        }
+    }
+}
